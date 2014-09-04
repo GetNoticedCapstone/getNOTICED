fb95727855be23ab7c5ab37032a582535fdf3429
David Landi <David Landi@DavidLandi-PC.home>
2014-09-03 22:51:45 -0400

    Merge origin/master
    Conflicts:
    	index.php
    	nbproject/project.xml

--- adminPage.php
+++ adminPage.php
@@ -9,61 +9,160 @@ This page will have tools such basic querys to view signup stats, member signup
 dates. It will also offer access to the database to allow a admin to modify a 
 customers data if they are having issues.
 -->
-<html lang="en">
-<!--{LOAD CSS & GOOGLE FONTS}............................................... -->    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/adminStyle.css" rel="stylesheet" media="all">
-        <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-        <title>get<i>N</i>OTICED</title>
-    </head>
-    <body>
-<!--{END CSS & GOOGLE FONTS}................................................ -->
-
-<!--{GLOBAL PHP}............................................................ -->
-        <?php
-            
-        ?>
-<!--{End PHP}............................................................... -->
-
-<!--{Main Navigation}....................................................... -->          
-        <div class="header">
-            <div class="container">
-                <nav class="navbar navbar-inverse" role="navigation">
-                    <div class="navbar-header">
-                        <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#mainNav">
-                            <span class="sr-only">Toggle</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a href="#" class="navbar-brand scrollTop">get<i>N</i>OTICED</a>
+<html lang="en">   
+<head><!--{LOAD CSS & GOOGLE FONTS}##########################################-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/adminStyle.css" rel="stylesheet" media="all">
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <title>getNOTICED</title>
+</head>
+<body><!--{END CSS & GOOGLE FONTS}###########################################-->
+<?php
+/* {Global PHP}############################################################## */
+/* {End Global PHP}########################################################## */    
+?>
+    <nav class="navbar navbar-inverse" role="navigation"><!--{NAVIGATION}####-->
+        <div class="container">
+            <div class="navbar-header">
+            <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#mainNav">
+                <span class="sr-only">Toggle</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="index.php" class="navbar-brand">get<i>N</i>OTICED</a>
+        </div>
+        <div id="mainNav" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="adminDashboard.php"><span class="glyphicon glyphicon-home"></span> Dashboard </a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-wrench"></span> Admin Tools <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#"><i class="fa fa-table fa-fw"></i>Tables</a></li>
+                        <li><a href="#"><i class="fa fa-edit fa-fw"></i> Forms</a></li>
+                    </ul>
+                </li>
+                <li><a href="signupPage.php">Logout</a></li>
+            </ul>
+        </div>                  
+        </div>              
+    </nav><!--{END NAVIGATION}###############################################-->
+    <div class="container"><!--{MAIN WRAPPER}################################-->
+        <!--{Section contains 2 rows 3 panels per row}-->
+        <div class="row">
+            <div class="col-sm-4 col-md-4 col-lg-4">
+                <div class="panel panelPictonBlue">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-user fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9">
+                                
+                            </div>
+                        </div>
                     </div>
-                    <div id="mainNav" class="collapse navbar-collapse">
-                        <ul class="nav navbar-nav">                           
-                            <li><a href="#" class="gnScroll" data-id="about">About</a></li>
-                            <li><a href="#" class="gnScroll" data-id="team">Team</a></li>
-                            <li><a href="#" class="gnScroll" data-id="contact">Contact</a></li>
-                            <li><a href="#" class="gnScroll">Sign up</a></li>
-                            <li><a href="#" class="gnScroll">Login</a></li>
-                        </ul>
+                    <div class="panel-footer">
+                    
                     </div>
-                </nav>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4">
+                <div class="panel panelShamrock">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-users fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                    
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4">
+                <div class="panel panelRed">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-trash-o fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                    
+                    </div>
+                </div>
             </div>
         </div>
-<!--{END NAVIGATION}........................................................ -->
-
-<!--{MAIN CONTENT}.......................................................... -->
-  
-<!--{END MAIN CONTENT}...................................................... -->
-
-<!--{lOAD JAVASCRIPT}....................................................... -->
-        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-        <script src="js/bootstrap.js"></script> 
-        <script src="js/pageScroll.js"></script>
-<!--{END JAVASCRIPT}........................................................ -->        
-    </body>
+        <div class="row">
+            <div class="col-sm-4 col-md-4 col-lg-4">
+                <div class="panel panelLightningYello">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-support fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                    
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4">
+                <div class="panel panelOrange">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                
+                            </div>
+                            <div class="col-xs-9">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                    
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4">
+                <div class="panel panelStudio">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                
+                            </div>
+                            <div class="col-xs-9">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!--{END MAIN WRAPPER}#############################################-->
+<!--{lOAD JAVASCRIPT}########################################################-->
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/bootstrap.js"></script> 
+<script src="js/pageScroll.js"></script>
+<!--{END JAVASCRIPT}#########################################################-->        
+</body>
 </html>
